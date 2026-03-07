7f0407f1688c105eec9ebbd12fa368e462c44ddc
nightmare-web <Taylor.133@yandex.ru>
2026-03-08 00:18:32 +0500

    Delete Диплом SHOP.CO directory

--- Диплом SHOP.CO/js/promo-banner.js
+++
@@ -1,6 +0,0 @@
-const promo = document.querySelector(".promo-banner");
-const closeBtn = document.querySelector(".promo-banner__button");
-
-closeBtn?.addEventListener("click", () => {
-	promo?.classList.add("promo-banner--hidden");
-});

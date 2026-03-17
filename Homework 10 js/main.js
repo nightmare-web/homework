5063bf53bfbb9e15fec41a080247a1bddd1e2664
nightmare-web <Taylor.133@yandex.ru>
2026-03-17 21:30:28 +0500

    Add files via upload

---
+++ Homework 10 js/main.js
@@ -0,0 +1,19 @@
+// Задача 1
+const firstName = "Alexandr";
+const lastName = "Uskov";
+const isStudent = true;
+console.log(firstName, lastName, isStudent);
+
+// Задача 2
+const age = 28;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+console.log(birthYear);
+
+// Задача 3
+const infoStudent = `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`;
+console.log(infoStudent);
+
+// Задача 4
+const result = "123456789falsetrue";
+console.log(result);

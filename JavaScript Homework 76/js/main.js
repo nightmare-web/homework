11b5942120df781e2bd4136e1114dd04ba1a986f
nightmare-web <Taylor.133@yandex.ru>
2026-03-23 21:50:26 +0500

    Add files via upload

---
+++ JavaScript Homework 76/js/main.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const todoKeys = {
+	id: "id",
+	text: "text",
+	is_completed: "is_completed",
+};
+
+const todos = [];
+
+const errTodoNotFound = (todoId) => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = (todos) =>
+	todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+	const newTodo = {
+		[todoKeys.id]: getNewTodoId(todos),
+		[todoKeys.text]: text,
+		[todoKeys.is_completed]: false,
+	};
+	todos.push(newTodo);
+	return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+	const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+
+	if (!todo) {
+		console.error(errTodoNotFound(todoId));
+		return null;
+	}
+	todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+	return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+	const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+	if (todoIndex === -1) {
+		console.error(errTodoNotFound(todoId));
+		return todos;
+	}
+	todos.splice(todoIndex, 1);
+	return todos;
+};
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+const formElement = document.querySelector(".form");
+const inputElement = document.querySelector(".input");
+const todosElement = document.querySelector(".todos");
+
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+const createTodoElement = (text) => {
+	const li = document.createElement("li");
+	li.classList.add("todo");
+
+	li.innerHTML = `
+  <div class="todo-text">${text}</div>
+   <div class="todo-actions">
+    <button class="button-complete button">&#10004;</button>
+    <button class="button-delete button">&#10006;</button>
+  </div>
+  `;
+
+	return li;
+};
+
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+const handleCreateTodo = (todos, text) => {
+	const newTodo = createTodo(todos, text);
+	const todoElement = createTodoElement(newTodo.text);
+	todosElement.prepend(todoElement);
+};
